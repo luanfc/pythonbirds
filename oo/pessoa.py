95ac687b63d68642907fb24ee673ab77aca7a6a0
Luan Carvalho <luan.carvalho@camara.leg.br>
2019-02-25 13:45:42 -0300

    Criado atributo complexo filhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__=='__main__':
-    p = Pessoa('Luan')
+    renzo = Pessoa(nome='Renzo')
+    p = Pessoa(renzo, nome='Luan')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
     print(p.nome)
-    print(p.idade)
+    for filho in p.filhos:
+        print(filho.nome)
 
